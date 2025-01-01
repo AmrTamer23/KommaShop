ef4c91ca7fdae8323f53107c954f5147334b63d6
AmrTamer23 <amrtamer23mj@gmail.com>
2025-01-01 04:49:08 +0200

    minor changes on header

--- komma-storefront/src/modules/layout/templates/nav/moving-banner.tsx
+++ komma-storefront/src/modules/layout/templates/nav/moving-banner.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 export const MovingBanner: React.FC = () => {
   return (
-    <div className="bg-black text-zinc-300  py-2 z-0 relative">
+    <div className="bg-black text-zinc-300 py-2 z-0 relative overflow-hidden">
       <div className="animate-marquee whitespace-nowrap flex gap-[50dvw]">
         <span>🎉 Free delivery on orders above 1000EGP 🚚</span>
         <span>🎉 Free delivery on orders above 1000EGP 🚚</span>
